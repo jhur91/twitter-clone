70a5b61af97a0e3d368b229aa8df5ae17fe6ba26
Jason Hur <jasonyhur@gmail.com>
2020-12-16 22:24:49 -0800

    tweetBox

--- src/Feed.js
+++ src/Feed.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Feed.css";
+import TweetBox from "./TweetBox";
 
 function Feed() {
   return (
-    <div>
+    <div className="feed">
       <div className="feed__header">
         <h2>Home</h2>
       </div>
+
+      <TweetBox />
     </div>
   );
 }
